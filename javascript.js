83727812014e4e14ac47f23147e619df0eb047f8
Simon Garding <simon.garding1@gmail.com>
2022-06-19 16:19:34 +0200

    Update

--- javascript.js
+++ javascript.js
@@ -1,3 +1,20 @@
 function openLinkedin(){
     window.open('https://www.linkedin.com/in/therese-brunzell-120aba/', '_blank')
 }
+
+function hideText(){
+    var x = document.getElementById("textDiv");
+    var y = document.getElementById("topName");
+
+    x.style.display = "none";
+    y.style.display = "none"; 
+
+}
+
+function showText(){
+    var x = document.getElementById("textDiv");
+    var y = document.getElementById("topName");
+    
+    x.style.display = "block";
+    y.style.display = "block"; 
+}
